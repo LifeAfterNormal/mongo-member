8f7607390703852d2ee3001aa0b37037295ff4a1
Mitch Ferrer <mitch@lifeafternormal.net>
2016-05-10 00:27:37 -0600

    adjusting output

--- tests/put_member.sh
+++ tests/put_member.sh
@@ -1,8 +1,8 @@
 set -e
 
 echo "Create the mongo"
-MONGO_MEMBER=$(docker run $DOCKER_IMAGE_NAME)
+MONGO_MEMBER=$(docker run ${DOCKER_IMAGE_NAME})
 
-echo $MONGO_MEMBER
+echo "Result: ${MONGO_MEMBER}"
 
 echo $MONGO_MEMBER | grep containerId
